772629b74cb87d6df4a52b4d11253dba16cf353d
Edmundo Mendiola <e_mendz@outlook.com>
2017-10-07 21:46:53 -0400

    Mendz.Data.SqlServer
    Provides a generic Mendz.Data-aware context for ADO.Net-compatible access to SQL Server databases.

---
+++ Mendz.Data.SqlServer/SqlServerDataSettingOption.cs
@@ -0,0 +1,18 @@
+﻿namespace Mendz.Data.SqlServer
+{
+    /// <summary>
+    /// Provides the data setting options for SQL Server access.
+    /// </summary>
+    public static class SqlServerDataSettingOption
+    {
+        /// <summary>
+        /// Gets or sets the connection string.
+        /// </summary>
+        public static string Name { get; set;  } = "SqlServerConnectionString";
+
+        /// <summary>
+        /// Gets or sets the alternative connection string.
+        /// </summary>
+        public static string AlternativeName { get; set; }  = "SqlServerExpressConnectionString";
+    }
+}
---
+++ Mendz.Data.SqlServer/SqlServerDbDataContext.cs
@@ -0,0 +1,57 @@
+﻿using Mendz.Data.Common;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace Mendz.Data.SqlServer
+{
+    public class SqlServerDbDataContext : DbDataContextBase
+    {
+        /// <summary>
+        /// Provides the database context for an SQL Server database.
+        /// </summary>
+        protected override IDbConnection BuildContext()
+        {
+            string name = SqlServerDataSettingOption.Name;
+            if (!DataSettingOptions.ConnectionStrings.ContainsKey(SqlServerDataSettingOption.Name))
+            {
+                if (DataSettingOptions.ConnectionStrings.ContainsKey(SqlServerDataSettingOption.AlternativeName))
+                {
+                    name = DataSettingOptions.ConnectionStrings[SqlServerDataSettingOption.AlternativeName];
+                }
+            }
+            return new SqlConnection(DataSettingOptions.ConnectionStrings[name]);
+        }
+
+        /// <summary>
+        /// Creates a context asynchronously.
+        /// </summary>
+        /// <remarks>This method must be called explicity before accessing the Context property.
+        /// By default, Context calls the synchronous CreateContext() method.
+        /// </remarks>
+        public async void CreateContextAsync()
+        {
+            if (_context == null)
+            {
+                _context = BuildContext();
+                await ((SqlConnection)_context).OpenAsync();
+            }
+        }
+
+        /// <summary>
+        /// Creates a context asynchronously with a cancellation token.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <remarks>This method must be called explicity before accessing the Context property.
+        /// By default, Context calls the synchronous CreateContext() method.
+        /// </remarks>
+        public async void CreateContextAsync(CancellationToken cancellationToken)
+        {
+            if (_context == null)
+            {
+                _context = BuildContext();
+                await ((SqlConnection)_context).OpenAsync(cancellationToken);
+            }
+        }
+    }
+}
